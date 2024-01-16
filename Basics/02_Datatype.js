616456c34640f275740c89fb49c0ab9d2d1b11d7
Aman_piyush <136111983+ASTRA-crypt@users.noreply.github.com>
2024-01-16 16:08:39 +0000

    datatypes

---
+++ Basics/02_Datatype.js
@@ -0,0 +1,29 @@
+"use strict" //treat all js code as anewer version
+
+// alert(3+3) we are using node js not browser
+// code readability is important
+
+//tc39.es original javascript standards documents
+
+console.log(3+3)
+
+console.log("Alex")
+
+console.log("Hello");
+
+let Name = "Aman"
+let age = 21
+let isLoggedIn = false
+let state;
+
+//Number => 2 powerof 53
+//bigint used for stock and financial market
+//string ==> ""
+//boolean ==> true | false
+//null ==> it is stand alone value  (null is objects)
+//undefined ==> variable is declared but value is not defined
+//symbol  ==> Find the unique
+
+
+//objects
+console.log(typeof "");
