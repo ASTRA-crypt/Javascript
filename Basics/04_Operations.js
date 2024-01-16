77a68360b77800fd20545ef09b3cae84e0e01d8d
Aman_piyush <136111983+ASTRA-crypt@users.noreply.github.com>
2024-01-16 16:57:21 +0000

    change

---
+++ Basics/04_Operations.js
@@ -0,0 +1,29 @@
+//important Operations
+
+let value = 3
+let negvalue  = -value
+console.log(negvalue);
+
+
+let str1 = "Hello"
+let str2 = "Aman"
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1"+ 2);
+console.log(2 + "1");
+console.log(1 + 2 + "2")
+
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
+//prefix
+
+let x = 3
+let y = x++
+console.log(y);
