5bcf21314eb9c08e688f3610841fbf87f56f98a7
Aman_piyush <136111983+ASTRA-crypt@users.noreply.github.com>
2024-01-16 16:25:44 +0000

    converstion of datatypes

---
+++ Basics/03_conversionOperations.js
@@ -0,0 +1,20 @@
+let score = "33" //"" is a string
+console.log(typeof (score));
+
+let valueNumber = Number(score) //convert string into number forcefully
+console.log(valueNumber);  //"33abvc" ==> NaN not a Number
+
+//"true " ==> true = 1 | false = 0
+
+let isLoggedIn = 1
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+//" " ==> False
+//"Aman" ==> true
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
