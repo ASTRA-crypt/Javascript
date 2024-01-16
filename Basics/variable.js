66ab16a17bfacc8ead2aaa5fbf9e9d66ca39fc0d
Aman_piyush <136111983+ASTRA-crypt@users.noreply.github.com>
2024-01-16 15:41:36 +0000

    container config done

---
+++ Basics/variable.js
@@ -0,0 +1,13 @@
+const accountId = 123456 //cannot change
+let accountGmail = "Alex@gmail.com"  //let is use to create a variable 
+var accountpassword = "098765432"  //variable can declare as two types "lets and var" but "var" is not use.
+accountCity = "Bihar" //without varible name we can write
+
+let accountState;
+
+// accountId = 2 //Not allowed to change because it is constant
+accountGmail="axl@gmail.com"
+accountpassword = "12345678"
+accountCity = "Ayodhya"
+console.log(accountId);
+console.table([accountId,accountGmail,accountCity,accountpassword,accountState]) //output in table format
