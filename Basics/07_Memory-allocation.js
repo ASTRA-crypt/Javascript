11e100ec820c86a7bd6ece0cfe5c7c745706cc8f
Aman_piyush <136111983+ASTRA-crypt@users.noreply.github.com>
2024-01-18 10:41:08 +0000

    Memory and String

---
+++ Basics/07_Memory-allocation.js
@@ -0,0 +1,29 @@
+// Memory working on javascript
+
+/*merory are two type:
+1.Stack -- primitivw=e type
+2.Heap  -- Non-premitive type
+
+
+*/
+
+let Name = "Alex"
+let anotherName = Name
+
+anotherName = "Alpha"
+console.log(Name);
+console.log(anotherName);
+
+
+let Alex = {
+    Name : "Alex",
+    age : 22,
+    email : "alex@google.com"
+}
+
+let Alpha = Alex
+console.log(Alpha);
+
+Alpha.email = "Alpha@google.com"
+console.log(Alpha.email);
+console.log(Alex.email);
---
+++ Basics/08_String.js
@@ -0,0 +1,25 @@
+const myName = "Alex"
+const repoCount = 50
+
+console.log(myName + repoCount + "Added");
+
+//Modern String
+
+console.log(`Hello my name is ${myName} and my repocount is ${repoCount}`);
+
+
+//another way to write String
+const myGame = new String ("Alex_CRYPR")
+console.log(myGame);
+
+console.log(myGame[0]);
+
+console.log(myGame.length);
+console.log(myGame.toUpperCase());
+console.log(myGame.charAt(8));
+console.log(myGame.indexOf('Y'));
+
+
+//Study about all methods onces
+//Trim work only on whiteline
+//split
