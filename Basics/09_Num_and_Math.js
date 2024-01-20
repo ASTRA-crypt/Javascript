0dbba83a29ce4cf0a6f4e3cf518060eb9fd7cf7f
Aman_piyush <136111983+ASTRA-crypt@users.noreply.github.com>
2024-01-20 19:17:28 +0000

    Num and Math

---
+++ Basics/09_Num_and_Math.js
@@ -0,0 +1,31 @@
+const score = 400
+ const balance = new Number(900)
+console.log(balance);
+console.log(score);
+
+
+console.log(balance.toString());
+console.log(typeof balance);
+
+console.log(balance.toFixed(2 ));
+
+//precision vlaue 
+const otherNumber = 124.1557
+console.log(otherNumber.toPrecision(4));
+console.log(otherNumber);
+
+
+const Hundred = 34009080
+console.log(Hundred.toLocaleString('en-IN'));
+
+
+
+//MIN  and MAX Value
+
+
+//******************************************************* MATHS ********************************************* */
+console.log(Math);
+
+console.log(Math.abs(-4)); //Negative convert into positive
+
+console.log(Math.round(4.6));
