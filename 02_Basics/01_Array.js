95187ee429f9f73795ef54907b99bca2538c1149
Aman_piyush <136111983+ASTRA-crypt@users.noreply.github.com>
2024-01-27 18:27:00 +0000

    Array

--- 02_Basics/01_Array.js
+++ 02_Basics/01_Array.js
@@ -1,14 +1,31 @@
 //Array
 
-const Array = [0,2,2,3,4,5,5,5]
-console.log(Array[5]);
+const Array = [2,4,6,6,8]
+// console.log(Array);
+// console.log(Array.toString());
 
-//Shallow copy -- copy of original array
-//Deep copy -- dont copy the original array
-
-const Hero = ["Hanuman" , "Shaktiman", "Nagraj"]
-console.log(Hero);
+// console.log(typeof Array);
 
 
+// //Shallows copies - same copy of original class
+// // Deep copes -- dont share same referance
 
-//Array Methods
+
+// //Methods
+// Array.pop() //it remove the last value from value
+// console.log(Array);
+
+// Array.unshift(0)  //unshift mainly not used 
+// Array.shift()
+
+// console.log(Array.includes(9)); //add in first
+// console.log(Array.indexOf());
+
+
+const myN1 = Array.slice(1, 3)
+console.log(myN1);
+
+const myN2 = Array.splice(1, 3)
+console.log("B" ,Array);
+console.log(myN2)
+
